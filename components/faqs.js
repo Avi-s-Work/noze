53134844bc4e708f803a08cbce64eaececa25699
avishekataur <avishekataur@gmail.com>
2022-07-20 11:27:23 +0600

    faqs_fetch_done

--- components/faqs.js
+++ components/faqs.js
@@ -1,34 +1,54 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
-export default function Faqs() {
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+/* export const getStaticProps = async () => {
+  const res = await fetch("/faqs.json");
+  const data = await res.json;
+  return {
+    props: {
+      faqs: data,
+    },
+  };
+}; */
+
+export default function Faqs({ faqs }) {
+  const { data, error } = useSWR("/faqs.json", fetcher);
+
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+
   return (
     <>
       <div className="px-24 py-16">
         <h1 className="pb-12 text-5xl text-slate-900 font-bold text-center">
           FAQs
         </h1>
-        <div className="collapse border border-purple-400 bg-base-100 rounded-3xl hover:shadow-xl hover:border-purple-900">
-          <input type="checkbox" />
-          <div className="collapse-title flex justify-between items-center text-xl px-8 py-6">
-            <span className="font-bold text-purple-900">
-              How is NOZE different from other air quality monitors?
-            </span>
-            <FontAwesomeIcon
-              className="text-purple-200 text-4xl"
-              icon={faPlus}
-            />
+        {data.map((faq, key) => (
+          <div
+            key={faq.id}
+            className="collapse border border-purple-400 bg-base-100 rounded-3xl hover:shadow-xl hover:border-purple-900 mb-6"
+          >
+            <input type="checkbox" />
+            <div className="collapse-title flex justify-between items-center text-xl px-8 py-6">
+              <span className="font-bold text-purple-900">{faq.question}</span>
+              <FontAwesomeIcon
+                className="text-purple-200 text-4xl"
+                icon={faPlus}
+              />
+            </div>
+            <div className="collapse-content px-8">
+              <p className="pt-4 pb-2 text-lg text-gray-500">{faq.answer}</p>
+              {faq.note ? (
+                <p className="pt-6 pb-2 text-lg text-gray-500">{faq.note}</p>
+              ) : (
+                <div></div>
+              )}
+            </div>
           </div>
-          <div className="collapse-content px-8">
-            <p className="pt-4 pb-2 text-lg text-gray-500">
-              The technology behind NOZE is based on a digital nose sensor (also
-              known as an e-nose). With this sensor, NOZE can precisely detect a
-              wide range of pollutants and chemicals. In contrast, almost all
-              other air quality monitors on the market today use a bundle of
-              off-the-shelf sensors that are limited in what they can detect and
-              can vary widely in quality, accuracy and reliability.
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
     </>
   );
